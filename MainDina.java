adb20b6eb41e8c5aa4a8be35fd33787183a95523
bima46bayu <113746309+bima46bayu@users.noreply.github.com>
2023-03-07 21:48:56 +0700

    Add files via upload

---
+++ MainDina.java
@@ -0,0 +1,15 @@
+public class MainDina {
+    public static void main(String[] args) {
+//        memanggil method daftar belanja
+        System.out.println("daftar belanjaan Dina");
+        daftarBarang daftar = new daftarBarang();
+        daftar.Belanja();
+//        memanggil method barang yang di beli
+        System.out.println("barang yang di beli Dina");
+        daftarBarang beli = new daftarBarang();
+        beli.Beli();
+//        memanggil metthod kondisi uang dina
+        daftarBarang total = new daftarBarang();
+        total.Total();
+    }
+}
---
+++ MainMainan.java
@@ -0,0 +1,6 @@
+public class MainMainan {
+    public static void main(String[] args) {
+        Mainan masukan = new Mainan();
+        masukan.kondisi();
+    }
+}
